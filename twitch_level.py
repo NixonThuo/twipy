2ebf3e10d2d4c0106df14dfe087ef3c50fcc9f76
Nixon Thuo <thuonixon808@gmail.com>
2017-12-31 10:33:46 +0300

    complete

--- twitch_level.py
+++ twitch_level.py
@@ -4,10 +4,19 @@ import datetime
 import threading
 
 
+offlineSeconds = 0
+
+
 def checkActivetUsers():
-    threading.Timer(5.0, checkActivetUsers).start()  # called every second
+    global offlineSeconds
+    twitchthread = threading.Timer(
+        5.0, checkActivetUsers)
+    if(offlineSeconds <= 600):
+        twitchthread.start()
+    else:
+        twitchthread.cancel()
     timenow = datetime.datetime.now()
-    f = open('twitch_active_viewers.txt', 'w')
+    f = open('twitch_active_viewers.txt', 'a+')
     print timenow
     f.write(timenow.strftime("%Y-%m-%d %H:%M:%S") + '\n')
     client = TwitchClient('k0tqqij2xwujktxleoxyj8i6fa9jzv')
@@ -18,8 +27,15 @@ def checkActivetUsers():
         f.write(user.name + ':' + user.id + '\n')
         user_id = user.id
     stream = client.streams.get_stream_by_user(user_id)
-    print('%s: %s' % (stream[u'game'], stream[u'viewers']))
-    f.write(stream[u'game'] + ':' + str(stream[u'viewers']) + '\n')
+    if isinstance(stream, dict):
+        offlineSeconds = 0
+        print('%s: %s' % (stream[u'game'], stream[u'viewers']))
+        f.write('\n Game -' + stream[u'game'] +
+                ': viewers - ' + str(stream[u'viewers']) + '\n\n')
+    else:
+        offlineSeconds = offlineSeconds + 5
+        print 'Channel Currently Offline'
+        f.write('Channel Currently Offline \n\n')
     f.close()
 
 
